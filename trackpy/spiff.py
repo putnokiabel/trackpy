e7e496ca9ec3410aedc2126236eec7e26b0bdf3d
Abel Putnoki <29568469+putnokiabel@users.noreply.github.com>
2026-01-26 22:45:26 +0100

    Add SPIFF correction

---
+++ trackpy/spiff.py
@@ -0,0 +1,64 @@
+import numpy as np
+from pandas import DataFrame
+import warnings
+
+
+def apply_spiff_correction(f: DataFrame, pos_columns=None) -> DataFrame:
+    """
+    Removes pixel bias in a given list of features (using a single-pixel interior filling function),
+    thereby improving sub-pixel accuracy.
+
+    Parameters
+    ----------
+    :param f: DataFrame
+    :param pos_columns: list of column names, optional
+    :return: DataFrame
+
+    Notes
+    ----------
+    The algorithm used is inspired by "Analysis and correction of errors in
+    nanoscale particle tracking using the Single-pixel interior filling function
+    (SPIFF) algorithm" by Yuval et al.
+    The accuracy of this algorithm improves with the number of features. When
+    tracking features across multiple frames (e.g. in a video), consider locating
+    the features across all features first (using tp.batch) before applying this function
+    (as opposed to applying this function for each individual frame).
+    If f contains less than 100 features, f is returned as-is, due to lack of data.
+    """
+    if len(f) < 100:
+        warnings.warn("Not enough features to apply pixel bias correction")
+        return f
+
+    if pos_columns is None:
+        if 'z' in f:
+            pos_columns = ['x', 'y', 'z']
+        else:
+            pos_columns = ['x', 'y']
+
+    f = f.copy()
+
+    # Correct each column individually (subject to further optimization,
+    # by assuming sub-pixel bias is equal across certain dimensions).
+    for col in pos_columns:
+        # Get the values as a numpy array
+        x = np.array(f[col])
+
+        # Get the sub-pixel values
+        spiff = x % 1
+
+        # Mirror the values around the center of the pixel
+        spiff_mirrored = np.where(spiff < 0.5, spiff, 1 - spiff)
+
+        # Sort the values for efficient search
+        spiff_sorted = np.sort(spiff_mirrored)
+
+        # Reverse any sub-pixel bias
+        spiff_corrected_low = np.searchsorted(spiff_sorted, spiff) / len(x) / 2
+        spiff_corrected_high = 1 - np.searchsorted(spiff_sorted, 1 - spiff) / len(x) / 2
+        spiff_corrected = np.where(spiff < 0.5, spiff_corrected_low, spiff_corrected_high)
+
+        # Add the sub-pixel value back to the original pixel position
+        x_corrected = np.floor(x) + spiff_corrected
+        f[col] = x_corrected
+
+    return f
---
+++ trackpy/tests/test_spiff.py
@@ -0,0 +1,45 @@
+import unittest
+
+from build.lib.trackpy.utils import default_pos_columns
+from trackpy.artificial import draw_array
+from trackpy.spiff import apply_spiff_correction
+from trackpy.tests.common import StrictTestCase, sort_positions
+import trackpy as tp
+import numpy as np
+
+class MyTestCase(StrictTestCase):
+    def _test_spiff(self, ndim):
+        # Draw an image with 200 features and some noise
+        expected, image = draw_array(200, 2, noise_level=1, ndim=ndim)
+
+        columns = default_pos_columns(ndim)
+
+        # Locate the features and calculate the deviation
+        features = tp.locate(image, diameter=5)
+        _, actual = sort_positions(features[columns].values, expected)
+        deviation = np.sqrt(np.mean(np.sum((actual - expected) ** 2, 1)))
+
+        # Apply SPIFF correction and calculate the deviation
+        corrected_features = apply_spiff_correction(features)
+        _, corrected = sort_positions(corrected_features[columns].values, expected)
+        deviation_corrected = np.sqrt(np.mean(np.sum((corrected - expected) ** 2, 1)))
+
+        # Verify that the SPIFF correction improves accuracy
+        assert (deviation_corrected < deviation)
+
+        # Verify that the SPIFF corrected features have less subpixel bias
+        hist_dev = np.std(
+            np.histogram(features[columns] % 1, bins=10, range=(0, 1))[0]
+        )
+        corrected_hist_dev = np.std(
+            np.histogram(corrected_features[columns] % 1, bins=10, range=(0, 1))[0]
+        )
+        assert (corrected_hist_dev < hist_dev)
+
+    def test_spiff_2d(self):
+        self._test_spiff(ndim=2)
+    def test_spiff_3d(self):
+        self._test_spiff(ndim=3)
+
+if __name__ == '__main__':
+    unittest.main()
